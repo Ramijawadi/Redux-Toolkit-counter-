2a4a3e8f67ece99725061b4029486e77d0a47ac0
Rami jawadi <ramijawadi104@gmail.com>
2022-11-23 23:21:10 +0100

    FIX : the first version fo counter using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import {increment , decrement ,incrementWithAmount} from "./Redux/counter"
+
 
 function App() {
+const count = useSelector((state)=> state.counter.value);
+const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> the counter is {count}</h1>
+<button onClick={()=> dispatch(increment())} >increment</button>
+<button  onClick={()=>dispatch( decrement())} >decrement</button>
+<button  onClick={()=>dispatch( incrementWithAmount(5))} >incrementWithAmount</button>
+
+
+
     </div>
   );
 }
